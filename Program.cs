56c7b06782904efe36243a1cb75c2d573571622f
Danil <Kutfilloev.danil@yandex.ru>
2022-08-11 13:42:21 +0500

    делал пуш. делал, а ни ничего не получается

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("enter a free-digit namber");
 int number = Convert.ToInt32(Console.ReadLine());
-int digit = number / 100;
-Console.WriteLine(digit);
+int digit = number / 010;
+Console.WriteLine(digit1);
 
